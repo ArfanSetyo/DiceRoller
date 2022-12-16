c651db58ff5a3573de7030f785c881e8a61dc738
ArfanSetyo <arfan.marsetyo@students.utdi.ac.id>
2022-12-16 14:21:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/diceroller/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+//menggunakan daftar status warna, Anda dapat memberikan warna yang berbeda pada setiap status//
+package com.example.diceroller.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
